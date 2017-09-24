e71697a43740d9af06b3814649546c6117a303c8
Hanahubuki <1780477179@qq.com>
2017-09-24 09:29:31 +0800

    first try

---
+++ Try/src/zpy/Demo.java
@@ -0,0 +1,13 @@
+package zpy;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("konichiwa");
+	}
+
+}
